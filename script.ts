a00c620493525f2027fb0cea17d1c5d6d1dbe3cd
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-12-05 11:21:13 +0100

    fixed bug

--- script.ts
+++ script.ts
@@ -300,8 +300,9 @@ function InputChanged() {
         let x: number = GenerateRandomNumber(canvasWidth * 0.15, canvasWidth * 0.85);
         let y: number = GenerateRandomNumber(canvasHeight * 0.15, canvasHeight * 0.85);
 
-        let vx: number = Number((Math.random() * Math.floor(Math.random() * (3)) - 1).toFixed(2)) * starting_velocity_multiplier;
-        let vy: number = Number((Math.random() * Math.floor(Math.random() * (3)) - 1).toFixed(2)) * starting_velocity_multiplier;
+        let vx: number = Number(((Math.random() * Math.floor(Math.random() * (3)) - 1) * starting_velocity_multiplier).toFixed(2));
+        let vy: number = Number((Math.random() * Math.floor(Math.random() * (3) * starting_velocity_multiplier) - 1).toFixed(2));
+
         let color: Color = Color.GenerateRandomColor();
         let mass: number = default_mass;
         if (random_mass_input.checked) {
